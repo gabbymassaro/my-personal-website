33b2127331da4cf0464629a5a4e87fbf2658332d
Gabriella Massaro <gmassaro807@gmail.com>
2024-02-29 11:21:01 -0500

    update nested class images to ids, hook up event listener to headshot

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ const h2 = document.createElement("h2");
 h2.textContent = "";
 
 document.querySelector("body").appendChild(h2);
+
+const headshot = document.getElementById('headshot');
+
+headshot.addEventListener('mouseover', function() {
+    headshot.style.opacity = '0.5';
+});
+
+headshot.addEventListener('mouseout', function() {
+    headshot.style.opacity = '1'; 
+});
