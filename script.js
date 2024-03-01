c09bf0eaebdc71e060fce496c6a3435b11521876
Gabriella Massaro <gmassaro807@gmail.com>
2024-02-29 21:58:56 -0500

    fun little addition to mouseover on headshot

--- script.js
+++ script.js
@@ -4,11 +4,14 @@ h2.textContent = "";
 document.querySelector("body").appendChild(h2);
 
 const headshot = document.getElementById('headshot');
+const message = document.getElementById('message');
 
 headshot.addEventListener('mouseover', function() {
     headshot.style.opacity = '0.5';
+    message.style.display='block';
 });
 
 headshot.addEventListener('mouseout', function() {
     headshot.style.opacity = '1'; 
+    message.style.display = 'none';
 });
