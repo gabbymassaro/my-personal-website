46b886deeb053fe74f307868c1eda98149e693d2
Gabriella Massaro <gmassaro807@gmail.com>
2024-02-29 23:08:48 -0500

    change contact file to about file, remove extras

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ document.querySelector("body").appendChild(h2);
 const headshot = document.getElementById('headshot');
 const message = document.getElementById('message');
 
+
 headshot.addEventListener('mouseover', function() {
     headshot.style.opacity = '0.5';
     message.style.display='block';
@@ -15,3 +16,4 @@ headshot.addEventListener('mouseout', function() {
     headshot.style.opacity = '1'; 
     message.style.display = 'none';
 });
+
